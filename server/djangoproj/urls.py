88a3993dfaf2aa03d9e8abd709fe51735466dd5e
KAESZAR <cesdev.s@outlook.com>
2025-04-07 10:25:05 -0300

    Ultimate changes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,4 +1,5 @@
-"""djangoproj URL Configuration
+"""
+djangoproj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,8 +19,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+ 
 
 urlpatterns = [
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
