aeffa9042774422073727cf3ffc3aa2650046b4f
KAESZAR <cesdev.s@outlook.com>
2025-04-08 13:48:17 -0300

    Create models in models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,27 @@
-# Uncomment the following imports before adding the Model code
+from django.db import models  
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+class CarMake(models.Model):  
+    name = models.CharField(max_length=100)  # Nombre de la marca  
+    description = models.TextField(blank=True)  # Descripción de la marca  
+    # Otros campos que se deseen incluir, por ejemplo:  
+    country_of_origin = models.CharField(max_length=100, blank=True)  # País de origen  
 
+    def __str__(self):  
+        return self.name  # Retorna el nombre como representación de cadena  
 
-# Create your models here.
+class CarModel(models.Model):  
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Relación de muchos a uno  
+    dealership_id = models.IntegerField()  # ID del concesionario  
+    name = models.CharField(max_length=100)  # Nombre del modelo  
+    CAR_TYPES = [  
+        ('SEDAN', 'Sedan'),  
+        ('SUV', 'SUV'),  
+        ('WAGON', 'Wagon'),  
+        # Agregar más opciones según sea necesario  
+    ]  
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')  # Tipo de coche  
+    year = models.DateField()  # Año del modelo (usamos DateField)  
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    def __str__(self):  
+        return f"{self.car_make} {self.name}"  # Retorna la marca y el nombre del modelo como representación de cadena 
