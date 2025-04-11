f66f7b34b5fb2705611215b165c1bb539adda868
KAESZAR <cesdev.s@outlook.com>
2025-04-11 11:22:21 -0300

    create path get_dealer_detail

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,6 +7,8 @@ from .views import login_user, logout_request
 
 app_name = 'djangoapp'
 urlpatterns = [
+    # Path get_dealer_details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details')
     # view get_dealership
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
