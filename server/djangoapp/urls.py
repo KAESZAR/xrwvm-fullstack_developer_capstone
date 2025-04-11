dae44a3ec71476a1787ee28781be2538f550310d
KAESZAR <cesdev.s@outlook.com>
2025-04-11 11:30:48 -0300

    Update commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
-    # path for dealer reviews view
+    # path for logout
     path(route='logout', view=views.logout_request, name='logout'), 
 
     # path for add a review view
