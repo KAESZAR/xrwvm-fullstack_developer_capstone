654f5a7a2e5bdd6f55453b0523a3f08a025462f6
KAESZAR <cesdev.s@outlook.com>
2025-04-11 11:42:24 -0300

    # path for add a review view

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,13 +9,17 @@ app_name = 'djangoapp'
 urlpatterns = [
     # Path get_dealer_reviews
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+
     # Path get_dealer_details
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details')
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
     # view get_dealership
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
     # path for registration
     path(route='registration/', view=views.registration, name='registration'),
+
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
@@ -23,5 +27,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'), 
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
