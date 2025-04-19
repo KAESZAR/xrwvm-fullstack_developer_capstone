ffd063660bfb219919f4ed8ca098e876b3206229
KAESZAR <cesdev.s@outlook.com>
2025-04-19 17:47:15 -0400

    Add and conf/compReact

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -6,10 +6,10 @@ import Dealer from "./components/Dealers/Dealer";
 function App() {
   return (
     <Routes>
-      <Route path="/dealer/:id" element={<Dealer/>} />
-      <Route path="/dealers" element={<Dealers/>} />
-      <Route path="/login" element={<LoginPanel />} />
+            <Route path="/dealer/:id" element={<Dealer/>} />
+            <Route path="/dealers" element={<Dealers/>} />
+            <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
 }
-export default App; 
+export default App;
