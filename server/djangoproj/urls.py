4bdf978802dcd7471a5b56e5cdb9138b459017bf
KAESZAR <cesdev.s@outlook.com>
2025-04-21 16:04:04 -0400

    Adding component React/postReview

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-
+        path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
         path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
         path('dealers/', TemplateView.as_view(template_name="index.html")),
         path('register/', TemplateView.as_view(template_name="index.html")),
@@ -15,7 +15,6 @@ urlpatterns = [
         path('admin/', admin.site.urls),
         path('djangoapp/', include('djangoapp.urls')),
         path('', TemplateView.as_view(template_name="Home.html")),
-
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
