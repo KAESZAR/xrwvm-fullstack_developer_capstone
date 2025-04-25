c6bb0e01aad31a761a7331f745cb5cc721d96057
KAESZAR <cesdev.s@outlook.com>
2025-04-25 13:31:29 -0300

    linting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,17 +1,14 @@
 import json
 import logging
-from datetime import datetime
 
-from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import CarMake, CarModel
 from .populate import initiate
-from .restapis import analyze_review_sentiments, get_request, post_review
+from .restapis import get_request, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
